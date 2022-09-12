723c16b77ade394a33c667f4ef6b6d0deb2720ee
xxavierargentino <xxavierargentino@hotmail.com>
2022-09-12 18:22:55 -0300

    configuradas las environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TYPEORM_CONFIG } from './config/constants';
+import databaseConfig from './config/database.config';
 import { DepartamentosModule } from './departamentos/departamentos.module';
 import { UsuarioModule } from './usuario/usuario.module';
 
@@ -10,25 +12,28 @@ import { UsuarioModule } from './usuario/usuario.module';
   imports: [
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'mysql',
-        //host: config.get<string>(DATABASE_HOST),
-        host: "bt92jm2wtirh9gsjubcj-mysql.services.clever-cloud.com",
-        //port: parseInt(config.get<string>(DATABASE_PORT),10),
-        port: 3306,
-        //username: config.get<string>(DATABASE_USERNAME),
-        username:"ubntojirihw5pi9t",
-        //password: config.get<string>(DATABASE_PASSWORD),
-        password:"pFmW47CldiR7WvE1cITp",
-        //database: config.get<string>(DATABASE_NAME),
-        database: "bt92jm2wtirh9gsjubcj",
-        entities: [__dirname + "./**/**/*.entity{.ts,.js}"],
-        autoLoadEntities: true,
-        synchronize: true
-      }) 
-    }),    
+        useFactory: (config: ConfigService) => config.get<TypeOrmModuleOptions>(TYPEORM_CONFIG)  
+
+    //   useFactory: (config: ConfigService) => ({
+    //     type: 'mysql',
+    //     //host: config.get<string>(DATABASE_HOST),
+    //     host: "bt92jm2wtirh9gsjubcj-mysql.services.clever-cloud.com",
+    //     //port: parseInt(config.get<string>(DATABASE_PORT),10),
+    //     port: 3306,
+    //     //username: config.get<string>(DATABASE_USERNAME),
+    //     username:"ubntojirihw5pi9t",
+    //     //password: config.get<string>(DATABASE_PASSWORD),
+    //     password:"pFmW47CldiR7WvE1cITp",
+    //     //database: config.get<string>(DATABASE_NAME),
+    //     database: "bt92jm2wtirh9gsjubcj",
+    //     entities: [__dirname + "./**/**/*.entity{.ts,.js}"],
+    //     autoLoadEntities: true,
+    //     synchronize: true
+    //   }) 
+     }),  
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [databaseConfig],
       envFilePath: '.env'
     }), DepartamentosModule, UsuarioModule, 
 
