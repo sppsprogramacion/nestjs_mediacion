ad89bfb4ad17f4b7e9f3152a64a718c7858cc482
xxavierargentino <xxavierargentino@hotmail.com>
2022-09-13 12:23:23 -0300

    correcciones en el port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SERVER_PORT } from './config/constants';
 
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = parseInt(configService.get<string>(SERVER_PORT),10 || 3000);
   // app.useGlobalFilters(new AllExceptionFilter());
   // app.useGlobalInterceptors(new TimeOutInterceptor());
   // app.useGlobalPipes(new ValidationPipe({
@@ -13,7 +18,9 @@ async function bootstrap() {
   //   forbidNonWhitelisted: true,
   // }));
   app.enableCors();
-  //await app.listen(process.env.PORT);  
-  await app.listen(3000);
+
+  await app.listen(port);  
+  const logger = new Logger();
+  logger.log(`corriendo en el servidor ${await app.getUrl()}`)
 }
 bootstrap();
