257628f7ddb1d7c404769ea5b9e34b7f74dd8d55
pedro <pedrodiaz0487@gmail.com>
2022-10-23 21:05:55 -0300

    habilitacion de endpoint usuarios centros

--- src/usuarios-centros/usuarios-centros.controller.ts
+++ src/usuarios-centros/usuarios-centros.controller.ts
@@ -8,38 +8,38 @@ import { UpdateUsuarioCentroDto } from './dto/update-usuario-centro.dto';
 export class UsuariosCentrosController {
   constructor(private readonly usuariosCentrosService: UsuariosCentrosService) {}
 
-  // @Post()
-  // create(@Body() data: CreateUsuarioCentroDto) {
-  //   return this.usuariosCentrosService.create(data);
-  // }
+  @Post()
+  create(@Body() data: CreateUsuarioCentroDto) {
+    return this.usuariosCentrosService.create(data);
+  }
 
-  // @Get()
-  // findAll() {
-  //   return this.usuariosCentrosService.findAll();
-  // }
+  @Get()
+  findAll() {
+    return this.usuariosCentrosService.findAll();
+  }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
+  @Get(':id')
+  findOne(@Param('id') id: string) {
     
-  //   if(isNaN(Number(id))) throw new NotFoundException("El id del usuario-centro debe ser un número.")
-  //   let idx: number = parseFloat(id);
-  //   if(!Number.isInteger(idx)) throw new NotFoundException("El id del usuario-centro debe ser un número entero.")
-  //   return this.usuariosCentrosService.findOne(idx);
-  // }
+    if(isNaN(Number(id))) throw new NotFoundException("El id del usuario-centro debe ser un número.")
+    let idx: number = parseFloat(id);
+    if(!Number.isInteger(idx)) throw new NotFoundException("El id del usuario-centro debe ser un número entero.")
+    return this.usuariosCentrosService.findOne(idx);
+  }
 
-  // @Put(':id')
-  // update(@Param('id') id: string, @Body() dataDto: UpdateUsuarioCentroDto) {
-  //   if(isNaN(Number(id))) throw new NotFoundException("El id debe ser un número.")
-  //   let idx: number = parseFloat(id);
-  //   if(!Number.isInteger(idx)) throw new NotFoundException("El id debe ser un número entero.")
-  //   return this.usuariosCentrosService.update(idx, dataDto);
-  // }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() dataDto: UpdateUsuarioCentroDto) {
+    if(isNaN(Number(id))) throw new NotFoundException("El id debe ser un número.")
+    let idx: number = parseFloat(id);
+    if(!Number.isInteger(idx)) throw new NotFoundException("El id debe ser un número entero.")
+    return this.usuariosCentrosService.update(idx, dataDto);
+  }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   if(isNaN(Number(id))) throw new NotFoundException("El id debe ser un número.")
-  //   let idx: number = parseFloat(id);
-  //   if(!Number.isInteger(idx)) throw new NotFoundException("El id debe ser un número entero.")
-  //   return this.usuariosCentrosService.remove(idx);
-  // }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    if(isNaN(Number(id))) throw new NotFoundException("El id debe ser un número.")
+    let idx: number = parseFloat(id);
+    if(!Number.isInteger(idx)) throw new NotFoundException("El id debe ser un número entero.")
+    return this.usuariosCentrosService.remove(idx);
+  }
 }
