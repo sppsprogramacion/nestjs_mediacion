326c95707bfd45f249458e9aa302b0f4b80cf659
pedrodiaz0487 <pedrodiaz0487@gmail.com>
2023-03-13 13:07:17 -0300

    validaciones de enteron en controlador categoria

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, Put, ParseIntPipe } from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
@@ -13,21 +13,25 @@ export class CategoriasController {
   }
   
 
-  @Get()
-  findAll() {
-    return this.categoriasService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.categoriasService.findAll();
+  // }
+
+  
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: number) {    
     
-    if(isNaN(Number(id))) throw new NotFoundException("El id de la categoría debe ser un número.")
-    let idx: number = parseFloat(id);
-    if(!Number.isInteger(idx)) throw new NotFoundException("El id de la categoría debe ser un número entero.")
-    return this.categoriasService.findOne(idx);
+    return this.categoriasService.findOne(id);
   }
 
-
+  //PARA RUTA NO DEFINIDA
+  // @Get('*')
+  // rutasNoDefinidas() {
+  //   throw new NotFoundException('No se encontro la ruta especificada. Verifique si la ruta es correcta');
+  // }
+  //FIN PARA RUTA NO DEFINIDA...........
 
   @Put(':id')
   update(@Param('id') id: string, @Body() dataDto: UpdateCategoriaDto) {
