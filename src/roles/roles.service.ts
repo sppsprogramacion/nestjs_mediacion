e546e8f3c0f31f46807c8e8193e7a6b370612071
pedro <pedrodiaz0487@gmail.com>
2024-08-05 20:51:39 -0300

    roles

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Rol } from './entities/role.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class RolesService {
+  constructor(
+    @InjectRepository(Rol)
+    private readonly rolRepository: Repository<Rol>
+  ){}
+  
   create(createRoleDto: CreateRoleDto) {
     return 'This action adds a new role';
   }
 
-  findAll() {
-    return `This action returns all roles`;
+  async findAll() {
+    return await this.rolRepository.findAndCount(
+      {
+          order:{
+              rol: "ASC"
+          }
+      }
+    );
   }
 
   findOne(id: number) {
