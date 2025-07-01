b12ad1f60dfee1abe6dcbe18c3bab7ac148d9a8e
Pedro <TuCorreo@example.com>
2025-07-01 13:47:57 -0300

    seguridad en main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,26 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SERVER_PORT } from './config/constants';
+//para configuracion de <<disable>> por seguridad en cabeceras
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule);
+  //NestExpressApplication permite usar <<disable>> para seguridad
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
-  const port = parseInt(configService.get<string>(SERVER_PORT),10 || 3000);
+  const port = parseInt(configService.get<string>(SERVER_PORT),10) || 3000;
+
+  //CONFIGURACION DE SEGURIDAD (VULNERABILIDADES detectadas EN CHECKEOS)
+  //Desactivar cabecera X-Powered-By
+  app.disable('x-powered-by');
+  //Configurar CORS de forma segura -   <<app.enableCors();>> sin argumentos permite cualquier origen
+  app.enableCors({
+    origin: ['https://bdmediacion.salta.gob.ar'], // o más de uno si tenés
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  });
+
   // app.useGlobalFilters(new AllExceptionFilter());
   // app.useGlobalInterceptors(new TimeOutInterceptor());
   app.useGlobalPipes(new ValidationPipe({
@@ -16,7 +30,6 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
-  app.enableCors();
 
   await app.listen(port);  
   const logger = new Logger();
